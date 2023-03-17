1d1cb144a226585c7dfcee90c3bd5d7b720691e5
kishor <kishorsn1221@gmail.com>
2023-03-17 10:57:46 +0530

    Componets

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,32 @@ import { useState } from 'react';
 
 import './App.css'
 // import Home from './components/Home';
-import Login from './components/Login';
-import Navbar from './components/Navbar';
-
-
+// import Login from './components/Login';
+// import Navbar from './components/Navbar';
+// import Pending from './components/Pending';
+// import Nav1 from './components/Nav1';
+// import Dash from './components/Dash';
+// import Profile from './components/Profile';
+import Home from './components/Home';
+// import Pending from './components/Pending';
+// import Verified from './components/Verfied';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
-      <Login/> 
+    {/* <Navbar/>
+      <Login/>  */}
      {/* <Home/> */}
+{/* <Nav1/>   checked */}
+{/* <Dash/> checked */}
+
+{/* <Pending/> 
+// <Verified/>  checked */}
+{/* <Login/> */}
+{/* <Profile/>  check*/}
+{/* <Home/> cHECKED */}
     </>
   )
 }
---
+++ src/components/Pending.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Dash.css";
+export default function Pending() {
+  return (
+    <>
+      
+          {/* <!-- Start of individual experience pending box --> */}
+          <div className="container d-lg-flex justify-content-around pending-bg-set text-center my-3">
+         
+            {/* <div className="mt-4 mb-4"><img className="mt-3" src="img/download (3).jpeg" alt=""/></div> */}
+            <div className="d-flex flex-column  mt-5  ">
+              <h3>Company Name</h3>
+              <h5>Working as FrontEnd Developer</h5>
+              <p>2014-2018</p> 
+            </div>
+          </div>
+          {/* <!-- End of individual experience  pending box --> */}
+    </>
+  )
+}
