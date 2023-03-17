6289e799f8068966f308d3448c01c6230c2a7bc8
Aniruddha Upadhya K <112508814+Aniruddha-Upadhya-K@users.noreply.github.com>
2023-03-17 16:21:02 +0530

    Reuploaded navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import './Navbar.css'
-import { Link } from "react-router-dom";
+import {Link, NavLink} from 'react-router-dom'
 
-
-function Navbar() {
-    const [IsCompany, setIsCompany] = useState(false);
+function Navbar({props}) {
 
   return (
     <>
     {/* <!-- Navbar start --> */}
     <nav className="navbar navbar-expand-lg nav-color fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand text-light" href="#"><h2> JobTicket</h2></a>
+          {/* <a className="navbar-brand text-light" href="#"><h2> JobTicket</h2></a> */}
+          {<NavLink to={"/"}>JobTicket</NavLink>}
           <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon "></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
            
           {/* <div className="collapse navbar-collapse" id="navbarResponsive"> */}
-              <ul className="navbar-nav ms-auto my-2 my-lg-0 text-white text-left">
-                  <li className="nav-item"><a className="nav-link text-white text-left nav-l  " href="#top">Home</a></li>
-                  <li className="nav-item"><a className="nav-link text-white nav-l text-left " href="#about">About</a></li>
-                  <li className="nav-item"><a className="nav-link text-white nav-l text-left" href="#contact">Contact</a></li>
-                  <li className="nav-item"><Link className="nav-link text-white" to={'/signup'}><button className="btn btn-outline-light ">Login/Sign Up</button></Link></li>          
+              <ul className="navbar-nav ms-auto my-2 my-lg-0 text-white">
+                  <li className="nav-item"><a className="nav-link text-white text-center nav-l  " href="#top">Home</a></li>
+                  <li className="nav-item"><a className="nav-link text-white nav-l text-center " href="#about">About</a></li>
+                  <li className="nav-item"><a className="nav-link text-white nav-l text-center" href="#contact">Contact</a></li>
+                  <li className="nav-item">
+                    <Link to="/signup/company">
+                      <button className="btn btn-outline-light text-white">Office Login/Sign Up</button>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/signup/user">
+                      <button className="btn btn-outline-light text-white">Employee Login/Sign Up</button>
+                    </Link>
+                  </li>
+          
               </ul>
           {/* </div> */}
           </div>
